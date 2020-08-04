f67c819462f2c118df8f8522487db0aa6ab0d96f
suites <woosiks.io@gmail.com>
2020-08-05 01:00:44 +0900

    [all] Init Project

---
+++ src/test/kotlin/com/undefined/platz/PlatzApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.undefined.platz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatzApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

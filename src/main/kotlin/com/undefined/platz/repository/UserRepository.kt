e1933c2f0680d0d96e71d935af2d6a31f31edbb8
suites <woosiks.io@gmail.com>
2020-08-19 02:07:12 +0900

    [all] 코드 정리

--- src/main/kotlin/com/undefined/platz/repository/UserRepository.kt
+++ src/main/kotlin/com/undefined/platz/repository/UserRepository.kt
@@ -3,4 +3,4 @@ package com.undefined.platz.repository
 import com.undefined.platz.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository: JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long>

07f6f1f5917799af22a05ea3ac8943682ce0cf97
yoon.homme <woosiks.io@gmail.com>
2020-08-19 02:13:16 +0900

    [all] rds connection 세팅 (#4)

--- src/main/kotlin/com/undefined/platz/domain/User.kt
+++ src/main/kotlin/com/undefined/platz/domain/User.kt
@@ -1,10 +1,12 @@
 package com.undefined.platz.domain
 
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @Entity
 data class User(
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long?,
-        @Column(name = "name")
         val name: String)
--- src/test/kotlin/com/undefined/platz/PlatzApplicationTests.kt
+++ src/test/kotlin/com/undefined/platz/PlatzApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class PlatzApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }

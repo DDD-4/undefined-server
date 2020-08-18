7100d62bb36858a747b7f9326f5722b7df036957
suites <woosiks.io@gmail.com>
2020-08-19 02:05:25 +0900

    [common, configuration] db connection 정보를 받아오기 위해 DataSource 별도로 작성

---
+++ src/main/kotlin/com/undefined/platz/configuration/DataSourceConfig.kt
@@ -0,0 +1,62 @@
+package com.undefined.platz.configuration
+
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import javax.sql.DataSource
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "entityManagerFactory",
+        transactionManagerRef = "transactionManager",
+        basePackages = ["com.undefined.platz"]
+)
+class DataSourceConfig {
+    @Autowired
+    val parameterStoreProperties = ParameterStorePropertiesConfig()
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    fun dataSource(): DataSource {
+        val dataSourceBuilder = DataSourceBuilder.create()
+        dataSourceBuilder.url("jdbc:mysql://${parameterStoreProperties.dbHost}:3306/platz?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false")
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver")
+        dataSourceBuilder.username(parameterStoreProperties.dbUserName)
+        dataSourceBuilder.password(parameterStoreProperties.dbPassword)
+        dataSourceBuilder.type(HikariDataSource::class.java)
+        return dataSourceBuilder.build()
+    }
+
+    protected fun jpaProperties(): Map<String, String> {
+        return hashMapOf(
+                "hibernate.physical_naming_strategy" to SpringPhysicalNamingStrategy::class.java.name,
+                "hibernate.implicit_naming_strategy" to SpringPhysicalNamingStrategy::class.java.name
+        )
+    }
+
+    @Bean
+    fun entityManagerFactory(
+            builder: EntityManagerFactoryBuilder): LocalContainerEntityManagerFactoryBean {
+        return builder
+                .dataSource(this.dataSource())
+                .packages("com.undefined.platz")
+                .persistenceUnit("platzPersistenceUnit")
+                .properties(jpaProperties())
+                .build()
+    }
+
+    @Bean
+    fun transactionManager(builder: EntityManagerFactoryBuilder): JpaTransactionManager {
+        return JpaTransactionManager(entityManagerFactory(builder).`object`!!)
+    }
+}

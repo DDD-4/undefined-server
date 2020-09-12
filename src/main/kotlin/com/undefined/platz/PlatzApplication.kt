e9c5ce2ea381ddcdef698b175814e3edceffa007
suites <woosiks.io@gmail.com>
2020-09-13 01:51:20 +0900

    [domain, graphql] BaseEntity 추가 및 DateTime Graphql Type 추가

--- src/main/kotlin/com/undefined/platz/PlatzApplication.kt
+++ src/main/kotlin/com/undefined/platz/PlatzApplication.kt
@@ -5,9 +5,11 @@ import com.undefined.platz.configuration.ParameterStoreConfigurationProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableConfigurationProperties(ParameterStoreConfigurationProperties::class, ApplicationConfigurationProperties::class)
 @SpringBootApplication
+@EnableJpaAuditing
 class PlatzApplication
 
 fun main(args: Array<String>) {

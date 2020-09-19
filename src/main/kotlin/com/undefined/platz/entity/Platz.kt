82cc193cc84a8c3a67d499e5c4e366f80f6c1383
suites <woosiks.io@gmail.com>
2020-09-19 17:44:51 +0900

    [entity] 현재 작성되어 있지 않은 entity에 대한 연관관계 제거

--- src/main/kotlin/com/undefined/platz/entity/Platz.kt
+++ src/main/kotlin/com/undefined/platz/entity/Platz.kt
@@ -3,18 +3,20 @@ package com.undefined.platz.entity
 import javax.persistence.Entity
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
-import javax.persistence.OneToMany
 
 @Entity
 data class Platz(
         @ManyToOne
         @JoinColumn(name = "user_id")
         val user: User,
-        val title: String?,
-        @OneToMany(mappedBy = "platz")
-        val tags: Set<Tag>,
-        @OneToMany(mappedBy = "platz")
-        val platzLikes: Set<PlatzLike>,
-        @OneToMany(mappedBy = "platz")
-        val platzPlaces: Set<PlatzPlace>
+        var title: String?
+        // TODO: Tag 작성시 맵핑 예정
+//        @OneToMany(mappedBy = "platz", fetch = FetchType.LAZY)
+//        val tags: Set<Tag>? = null,
+        // TODO: PlatzLike 작성시 맵핑 예정
+//        @OneToMany(mappedBy = "platz")
+//        val platzLikes: Set<PlatzLike>? = null,
+        // TODO: Place 작성시 맵핑 예정
+//        @OneToMany(mappedBy = "platz")
+//        val platzPlaces: Set<PlatzPlace>? = null
 ) : BaseEntity()
